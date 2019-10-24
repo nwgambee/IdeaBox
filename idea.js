0fc65c0316f278efff57b1cbf1ab2cbaf1f21964
Noah Gambee <noah@gambee.dev>
2019-10-24 16:23:32 -0600

    Work on adding cards more dynamically

--- idea.js
+++ idea.js
@@ -1,11 +1,20 @@
 class Idea {
   constructor(title, body) {
-    // this.id = id;
+    this.id = Date.now();
     this.title = title;
     this.body = body;
     this.star = false;
   }
   isStarred() {
     this.star = true;
+  }
+  saveToStorage() {
+
+  }
+  deleteFromStorage() {
+
+  }
+  updateIdea() {
+
   }
 }
--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var bodyInput = document.querySelector('.body-input');
 var saveBtn = document.querySelector('#save-btn');
 var form = document.querySelector('.form');
 var cardSection = document.querySelector('.card-section');
+var cardData = [];
 
 
 form.addEventListener('input', toggleDisableSave);
@@ -22,30 +23,51 @@ form.addEventListener('click', addCard);
 function addCard(event) {
   if (event.target.id === 'save-btn') {
     instaniateNewIdea();
-    cardSection.innerHTML += `<div class="card">
-        <header>
-            <button id="star-btn" type="button" name="star"></button>
-            <button id="delete-btn" type="button" name="delete"></button>
-        </header>
-        <div class="card-body">
-            <h2>${titleInput.value}</h2>
-            <p id="card-body-p">${bodyInput.value}</p>
-        </div>
-        <footer class="comment">
-            <button id="comment-btn" type="button" name="button"></button>
-            <p id="comment">Comment</p>
-        </footer>
-    </div>`;
-    console.log('adding a new card!!');
+    addNewCard();
+
+
+    // cardSection.innerHTML += `<div class="card">
+    //     <header>
+    //         <button id="star-btn" type="button" name="star"></button>
+    //         <button id="delete-btn" type="button" name="delete"></button>
+    //     </header>
+    //     <div class="card-body">
+    //         <h2>${titleInput.value}</h2>
+    //         <p id="card-body-p">${bodyInput.value}</p>
+    //     </div>
+    //     <footer class="comment">
+    //         <button id="comment-btn" type="button" name="button"></button>
+    //         <p id="comment">Comment</p>
+    //     </footer>
+    // </div>`;
     if((titleInput !== '') && (bodyInput !== '')) {
       titleInput.value = '';
       bodyInput.value = '';
       toggleDisableSave();
 }}
-
 };
 
 function instaniateNewIdea() {
   var newCard = new Idea(titleInput.value, bodyInput.value);
+  cardData.push(newCard);
   console.log(newCard);
+  console.log(cardData);
+  return newCard;
+}
+
+function addNewCard() {
+  cardSection.innerHTML += `<div class="card">
+      <header>
+          <button id="star-btn" type="button" name="star"></button>
+          <button id="delete-btn" type="button" name="delete"></button>
+      </header>
+      <div class="card-body">
+          <h2>${cardData[0].title}</h2>
+          <p id="card-body-p">${cardData[0].body}</p>
+      </div>
+      <footer class="comment">
+          <button id="comment-btn" type="button" name="button"></button>
+          <p id="comment">Comment</p>
+      </footer>
+  </div>`
 }
