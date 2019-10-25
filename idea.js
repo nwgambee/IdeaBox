8cfd02c87d8796694fb2c21b080fc07d0ab3656e
Noah Gambee <noah@gambee.dev>
2019-10-24 19:53:56 -0600

    Update idea card functionality and refactor some functions

--- idea.js
+++ idea.js
@@ -1,8 +1,21 @@
 class Idea {
     constructor(title, body) {
-        this.title = title;
-        this.body = body;
-        this.id = Date.now();
-        this.starred = false;
+      this.title = title;
+      this.body = body;
+      this.id = Date.now();
+      this.starred = false;
     }
+    starCard() {
+      this.starred = true;
+    }
+    saveToStorage() {
+
+    }
+    deleteFromStorage() {
+
+    }
+    updateIdea() {
+
+    }
+
 }
--- main.js
+++ main.js
@@ -2,9 +2,10 @@ var titleInput = document.querySelector('.title-input');
 var bodyInput = document.querySelector('.body-input');
 var saveBtn = document.querySelector('#save-btn');
 var form = document.querySelector('.form');
+var cardSection = document.querySelector('.card-section');
+var ideas = [];
 
-
-
+// disable save button until forms have content
 form.addEventListener('input', toggleDisableSave);
 
 function toggleDisableSave(event) {
@@ -20,12 +21,40 @@ function toggleDisableSave(event) {
 form.addEventListener('click', addCard);
 
 function addCard(event) {
-    if (event.target.id === 'save-btn') {
-        console.log('adding a new card!!');
-        if ((titleInput !== '') && (bodyInput !== '')) {
-            titleInput.value = '';
-            bodyInput.value = '';
-            toggleDisableSave();
-        }
-    }
+  if (event.target.id === 'save-btn') {
+    var idea = new Idea(titleInput.value, bodyInput.value);
+    ideas.push(idea);
+    addIdeaCard();
+
+// reset inputs on 'Save' (could be its own method)
+  if((titleInput !== '') && (bodyInput !== '')) {
+      titleInput.value = '';
+      bodyInput.value = '';
+      toggleDisableSave();
+  }}
 };
+
+function addIdeaCard() {
+  var lastIdea = ideas[ideas.length - 1];
+  cardSection.innerHTML += `<div class="card">
+      <header>
+          <button id='${lastIdea.id}' type="button" name="star"></button>
+          <button id="delete-btn" type="button" name="delete"></button>
+      </header>
+      <div class="card-body">
+          <h2>${lastIdea.title}</h2>
+          <p id="card-body-p">${lastIdea.body}</p>
+      </div>
+      <footer class="comment">
+          <button id="comment-btn" type="button" name="button"></button>
+          <p id="comment">Comment</p>
+      </footer>
+  </div>`
+}
+
+cardSection.addEventListener('click', starIdea);
+
+function starIdea() {
+  // use bubbling to target the star button on a specific card, and store the ID of that card in a variable.
+  // With that variable, access the specific idea instance within the ideas array and run the starCard() method.
+}
