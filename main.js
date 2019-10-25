ed732fce6e9aa849ec8c97823e4a2451a2ccd65d
Cody Smith <codysmith2814@gmail.com>
2019-10-24 19:41:20 -0600

    Adds cards to the page by pulling information from global array

--- main.js
+++ main.js
@@ -2,12 +2,16 @@ var titleInput = document.querySelector('.title-input');
 var bodyInput = document.querySelector('.body-input');
 var saveBtn = document.querySelector('#save-btn');
 var form = document.querySelector('.form');
+var ideas = [];
+var cardSection = document.querySelector('.card-section')
 
 
 
 form.addEventListener('input', toggleDisableSave);
+form.addEventListener('click', addCard);
 
-function toggleDisableSave(event) {
+
+function toggleDisableSave() {
     if (titleInput.value !== "" && bodyInput.value !== "") {
         saveBtn.classList.add('save-btn')
         saveBtn.disabled = false;
@@ -17,15 +21,43 @@ function toggleDisableSave(event) {
     }
 };
 
-form.addEventListener('click', addCard);
 
 function addCard(event) {
     if (event.target.id === 'save-btn') {
-        console.log('adding a new card!!');
+        instantiateIdea();
+        displayCard();
         if ((titleInput !== '') && (bodyInput !== '')) {
             titleInput.value = '';
             bodyInput.value = '';
             toggleDisableSave();
         }
     }
-};
+}
+
+function instantiateIdea() {
+    var newCard = new Idea(titleInput.value, bodyInput.value);
+    console.log(newCard)
+    ideas.push(newCard)
+    console.log(ideas)
+}
+
+function displayCard() {
+    for (var i = 0; i < ideas.length; i++) {
+        var title = ideas[i].title;
+        var body = ideas[i].body;
+    }
+    cardSection.innerHTML += `<div class="card">
+        <header>
+            <button id="star-btn" type="button" name="star"></button>
+            <button id="delete-btn" type="button" name="delete"></button>
+        </header>
+        <div class="card-body">
+            <h2>${title}</h2>
+            <p id="card-body-p">${body}</p>
+        </div>
+        <footer class="comment">
+            <button id="comment-btn" type="button" name="button"></button>
+            <p id="comment">Comment</p>
+        </footer>
+        </div>`
+}
