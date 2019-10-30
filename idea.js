3cd0f2eaee57c53e1c9b50772e6ffe42a8e37b5c
Cody Smith <codysmith2814@gmail.com>
2019-10-29 18:28:04 -0600

    Cleans up some commented out code

--- idea.js
+++ idea.js
@@ -6,27 +6,8 @@ class Idea {
         this.starred = false;
     }
     starCard() {
-        console.log('yo')
         this.starred = !this.starred
-            // use individual ID being created for each card
-            // use that ID to iterate through the array
-            // find which instance contains that ID
-            // run starCard() on that instance
-            //     console.log('event: ', event)
-            //     for (var i = 0; i < event.length; i++) {
-            //         console.log(event[i].id, this.id)
-            //         if (event[i].id === this.id) {
-            //             this.starred = !this.starred;
-            //         }
-            //     }
-            // }
-            // saveToStorage(ideasArr) {
-            //     var stringifiedIdeas = JSON.stringify(ideas);
-            //     localStorage.setItem('ideas', stringifiedIdeas)
-            //     console.log(stringifiedIdeas)
-            // }
     }
-
 
     deleteFromStorage() {
 
